3a38978d1eaacb5d6428bcf78a491ce79dbcaf6b
Vivianne <viviannevilar@gmail.com>
2020-10-03 10:10:46 +0800

    changes to users admin

--- projects/admin.py
+++ projects/admin.py
@@ -6,8 +6,11 @@ class ProjectAdmin(admin.ModelAdmin):
     model = Project
     list_display = ['title','id','owner', 'is_open']
 
+class PledgeAdmin(admin.ModelAdmin):
+    model = Pledge
+    list_display = ['id','supporter', 'date_sent']
 
 admin.site.register(Project,ProjectAdmin)
 admin.site.register(Category)
-admin.site.register(Pledge)
+admin.site.register(Pledge,PledgeAdmin)
 admin.site.register(Favourite)
