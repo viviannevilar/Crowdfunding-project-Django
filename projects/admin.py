876c62ac73f52c09902bc95d87682004411225b0
viviannevilar <viviannevilar@gmail.com>
2020-11-23 09:26:22 +0800

    Update admin.py

--- projects/admin.py
+++ projects/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from .models import Project, Category, Pledge, Favourite
 
-
+@admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     model = Project
     list_display = ['title','id','owner', 'is_open']
+    #fields = ('title', 'id', 'owner', 'favouriters')
 
 
 class PledgeAdmin(admin.ModelAdmin):
     model = Pledge
     list_display = ['id','supporter', 'project', 'date_sent', 'project_owner']
 
-admin.site.register(Project,ProjectAdmin)
+#admin.site.register(Project,ProjectAdmin)
 admin.site.register(Category)
 admin.site.register(Pledge,PledgeAdmin)
 admin.site.register(Favourite)
